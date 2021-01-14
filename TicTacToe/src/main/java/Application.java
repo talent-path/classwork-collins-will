1218cdc8ec4ebfddb196e0f312bb888ecf24d49d
William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>
2021-01-14 15:37:36 -0500

    Better looking board

--- TicTacToe/src/main/java/Application.java
+++ TicTacToe/src/main/java/Application.java
@@ -335,14 +335,23 @@ public class Application {
     }
 
     public static void printBoard(char[] board) {
-        for (int i = 0; i < 9; i++) {
+        /*for (int i = 0; i < 9; i++) {
             System.out.print(board[i] + " ");
             if ((i + 1) % 3 == 0) {
                 System.out.println();
             }
         }
 
-        System.out.println();
+        System.out.println();*/
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + board[3 * i + j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 
     public static boolean checkWinner(char[] board) {
